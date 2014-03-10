cfd8a91cfe32d5619db7c4eb73b1975c3545f951
LukaszCaputa <lukasz@lukaszcaputa.eu>
2014-03-10 20:00:09 +0100

    Changing main view
    new icons

--- app/view/Main.js
+++ app/view/Main.js
@@ -2,15 +2,14 @@ Ext.define('SenchaHomework.view.Main', {
     extend: 'Ext.tab.Panel',
     xtype: 'main',
     requires: [
-        'Ext.TitleBar',
-        'Ext.Video'
-    ],
+        'Ext.TitleBar'
+        ],
     config: {
         tabBarPosition: 'bottom',
 
         items: [
             {
-                title: 'Welcome',
+                title: 'Main',
                 iconCls: 'home',
 
                 styleHtmlContent: true,
@@ -19,29 +18,49 @@ Ext.define('SenchaHomework.view.Main', {
                 items: {
                     docked: 'top',
                     xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
+                    title: 'Minimalistic webpage for homework'
                 },
 
                 html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
+                    "Main screen of the application"
                 ].join("")
             },
             {
-                title: 'Get Started',
-                iconCls: 'action',
+                title: 'Page 2',
+                iconCls: 'compose',
+                styleHtmlContent: true,
 
                 items: [
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
+                        title: 'Page 2'
+                    }
+                ],
+
+                html: [
+                "test"
+                ].join("")
+            },
+            {
+                title: 'Page 3',
+                iconCls: 'star',
+                items: [
                     {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Page 3'
+                    }
+                ]
+            },
+            {
+                title: 'Page 4',
+                iconCls: 'team',
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Page 4'
                     }
                 ]
             }
